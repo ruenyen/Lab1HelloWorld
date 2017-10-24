aa81c0a2c71eeac60915ab96c1fdd1c293a7e8ff
ruenyen <chinry-pa15@student.tarc.edu.my>
2017-10-24 12:32:39 +0800

    Initial

---
+++ app/src/main/java/my/edu/tarc/lab1helloworld/MainActivity.java
@@ -0,0 +1,44 @@
+package my.edu.tarc.lab1helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    //Global or module level variable
+    private EditText editTextName, editTextAge;
+    private TextView textViewMessage;
+
+    //onCreate = main() function
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Display the UI
+        //R = resources class
+        setContentView(R.layout.activity_main);
+
+        //Linking UI to program
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextAge = (EditText) findViewById(R.id.editTextAge);
+        textViewMessage = (TextView) findViewById(R.id.textViewMessage);
+    }
+
+    public void displayMessage(View view){
+        String name;
+        int age;
+        name = editTextName.getText().toString();
+        age = Integer.parseInt(editTextAge.getText().toString());
+        age++;
+        textViewMessage.setText("Hi " + name + ", you will be " + age + " years old in 2018.");
+    }
+
+    public void resetInput(View view){
+        editTextName.setText("");
+        editTextAge.setText("");
+        textViewMessage.setText("");
+    }
+}
